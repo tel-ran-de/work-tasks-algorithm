f939ce4560f908787d34798aa163201207c54c6b
Aleks Reingand <aleks.r@siltatel.fi>
2023-03-17 20:16:29 +0200

    added solution hw lecture #9

---
+++ src/main/java/org/telran/lecture_9_linked/Node.java
@@ -0,0 +1,27 @@
+package org.telran.lecture_9_linked;
+
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
